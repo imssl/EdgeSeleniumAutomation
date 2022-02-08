da231dccee7ea4a59e15596a0b1ddda9156d2fe1
İskender Mert Soysal <iskendersoysal@gmail.com>
2022-02-08 10:59:15 +0200

    Update ChromeDesktop.py

--- ChromeDesktop.py
+++ ChromeDesktop.py
@@ -1,5 +1,5 @@
 # Purpose of this tool is to execute automated playback tests
-# with given license server API, test vectors and tokens.
+# with given license server API, test medias and tokens.
 # pip3 install selenium pillow opencv-python
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
